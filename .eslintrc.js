f8624ecfb78fcd4137ed9c8ac2e81508d60f7bd6
Dima Petukhov <dphov@yahoo.com>
2023-01-24 14:33:28 +0100

    chore: add developer experience tooling: eslint, prettier, commitlint, strict tsconfig, husky

---
+++ .eslintrc.js
@@ -0,0 +1,39 @@
+module.exports = {
+  parser: "@typescript-eslint/parser",
+  parserOptions: {
+    ecmaVersion: 12,
+    sourceType: "module",
+  },
+  plugins: ["@typescript-eslint", "jest"],
+  extends: [
+    "eslint:recommended",
+    "plugin:@typescript-eslint/recommended",
+    "prettier",
+  ],
+  rules: {
+    "@typescript-eslint/no-unused-vars": "error",
+    // to enforce using type for object type definitions, can be type or interface
+    "@typescript-eslint/consistent-type-definitions": ["error", "type"],
+  },
+
+  env: {
+    node: true,
+    es2021: true,
+    "jest/globals": true,
+  },
+  overrides: [
+    {
+      files: ["**/**.spec.ts"],
+      plugins: ["jest"],
+      extends: ["plugin:jest/recommended"],
+      rules: {
+        "jest/prefer-expect-assertions": "off",
+        "jest/no-disabled-tests": "warn",
+        "jest/no-focused-tests": "error",
+        "jest/no-identical-title": "error",
+        "jest/prefer-to-have-length": "warn",
+        "jest/valid-expect": "error",
+      },
+    },
+  ],
+};
